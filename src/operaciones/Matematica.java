a4e42ec850c6f0014d235725f6e92444e3ce8b52
Ivan jimenez <aivan9403@gmail.com>
2021-06-02 11:28:45 -0500

    v3 definir metodos con parametros

--- src/operaciones/Matematica.java
+++ src/operaciones/Matematica.java
@@ -5,13 +5,17 @@ public class Matematica {
     int a;
     int b;
     public void sumar(){
-        int resultado = a+b;
+        int resultado = this.a + this.b;
         System.out.println("El resultado es "+resultado);
     }
     public int sumar1(){
-        int resultado = a + b;
+        int resultado = this.a + this.b;
         return resultado;
     }
-    
+    public void sumar2(int arg1, int arg2){
+        this.a = arg1;
+        this.b = arg2;
+        System.out.println("El resultado del metodo con parametros es "+(this.a+this.b));
+    }
 }
     
--- src/operaciones/PruebaMatematica.java
+++ src/operaciones/PruebaMatematica.java
@@ -5,12 +5,14 @@ package operaciones;
 public class PruebaMatematica {
     public static void main(String[] arg){
         Matematica ob1 = new Matematica();
+        var cadena = "Hola curso";
         ob1.a = 25;
         ob1.b = 35;
         ob1.sumar();
-        
+        //Crear metodo con parametros
         int res = ob1.sumar1();
         System.out.println("res - " + res);
         System.out.println("El resultado de la suma es "+ob1.sumar1());
-        }
+        ob1.sumar2(10, 15);
+    }
 }
